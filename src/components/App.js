a678f8af275e93ee91c8c6c424b1c29742679596
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-08 19:31:01 +0530

    navigation with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const PageOne = () => {
+    return <div>
+        pageone
+    <Link to='/2' >pagetwo</Link>
+    </div>
+}
+
+const PageTwo = () => {
+    return <div>pageTwo .......
+    <Link to='/' >pageone</Link>
+    </div>
+}
 
 const App = () => {
     return (
         <div>
-            app
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={PageOne} />
+                    <Route path='/2' component={PageTwo} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
