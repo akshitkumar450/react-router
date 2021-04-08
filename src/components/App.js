45df2c753493074e796b78ee99d9ff1536b2dcad
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-08 21:38:30 +0530

    navigation done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-const PageOne = () => {
-    return <div>
-        pageone
-    <Link to='/2' >pagetwo</Link>
-    </div>
-}
-
-const PageTwo = () => {
-    return <div>pageTwo .......
-    <Link to='/' >pageone</Link>
-    </div>
-}
+import StreamShow from './streams/StreamShow'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import Header from './Header'
 
 const App = () => {
     return (
-        <div>
+        <div className='ui container'>
             <BrowserRouter>
                 <div>
-                    <Route path='/' exact component={PageOne} />
-                    <Route path='/2' component={PageTwo} />
+                    <Header />
+                    <Route path='/' exact component={StreamList} />
+                    <Route path='/streams/new' exact component={StreamCreate} />
+                    <Route path='/streams/edit' exact component={StreamEdit} />
+                    <Route path='/streams/delete' exact component={StreamDelete} />
+                    <Route path='/streams/show' exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
