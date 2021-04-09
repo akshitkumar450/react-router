707cc398888c3629e5db5478431e6c5d26d43514
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-10 00:05:27 +0530

    handling form submission

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,23 +4,33 @@ class StreamCreate extends React.Component {
 
     renderInput(formProps) {
         // console.log(formProps);
-        return <input
-            {...formProps.input}
-        // value={formProps.input.value}
-        // onChange={formProps.input.onChange}
-        />
+        return (
+            <div className='field'>
+                <labe>{formProps.label}</labe>
+                <input {...formProps.input} />
+            </div>
+        )
     }
+
+    // will run when the form submit and the data of Fields will be available here
+    onSubmit(formValues) {
+        // e.preventDefault() // no need with redux form ,done automatically
+        console.log(formValues);
+    }
+
     render() {
         // console.log(this.props);
         return (
-            <form>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
                 <Field
-                    name='title'   //any name of the property (any) required
+                    name='title'
+                    label='enter title'
+                    // as the renderInput calls it will pass a set of properties as a props to that fn
                     component={this.renderInput}
                 />
 
-                <Field name='description' component={this.renderInput} />
-
+                <Field name='description' label='enter description' component={this.renderInput} />
+                <button className='ui button primary'>submit</button>
             </form>
         )
     }
