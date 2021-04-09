cf6320c14f7fc9b5e247a688d958922305d0ebee
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-09 23:32:21 +0530

    handling input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 class StreamCreate extends React.Component {
+
+    renderInput(formProps) {
+        // console.log(formProps);
+        return <input
+            {...formProps.input}
+        // value={formProps.input.value}
+        // onChange={formProps.input.onChange}
+        />
+    }
     render() {
         // console.log(this.props);
         return (
             <form>
                 <Field
                     name='title'   //any name of the property (any) required
+                    component={this.renderInput}
                 />
 
-                <Field name='description' />
+                <Field name='description' component={this.renderInput} />
 
             </form>
         )
@@ -17,5 +27,5 @@ class StreamCreate extends React.Component {
 }
 
 export default reduxForm({
-    form: 'streamCreate'  // key should be form and  value can be any thing (string )
+    form: 'streamCreate'  // key should be form and  value can be any thing (string)
 })(StreamCreate)
