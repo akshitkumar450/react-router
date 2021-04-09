20d36e6a34dc4135b61522ce74af4b73800762f7
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-09 23:00:47 +0530

    using redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+class StreamCreate extends React.Component {
+    render() {
+        // console.log(this.props);
+        return (
+            <form>
+                <Field
+                    name='title'   //any name of the property (any) required
+                />
 
-const StreamCreate = () => {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    )
+                <Field name='description' />
+
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+export default reduxForm({
+    form: 'streamCreate'  // key should be form and  value can be any thing (string )
+})(StreamCreate)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import authReducer from './authReducers'
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    // for storing form data in redux store  
+    // key should be form only
+    form: formReducer
 })
