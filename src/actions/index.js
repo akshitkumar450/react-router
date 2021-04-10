ae874e6be47a313aa44bd08cb1725c00a48b613e
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-11 00:00:38 +0530

    created all action creaters

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,3 +22,42 @@ export const createStream = (formValues) => {
         })
     }
 }
+
+export const fetchStreams = () => {
+    return async (dispatch) => {
+        const response = await axios.get('http://localhost:3001/streams')
+        dispatch({
+            type: 'FETCH_STREAMS',
+            payload: response.data
+        })
+    }
+}
+export const fetchStream = (id) => {
+    return async (dispatch) => {
+        const response = await axios.get(`http://localhost:3001/streams/${id}`)
+        dispatch({
+            type: 'FETCH_STREAM',
+            payload: response.data
+        })
+    }
+}
+
+export const editStream = (id, formValues) => {
+    return async (dispatch) => {
+        const response = await axios.put(`http://localhost:3001/streams/${id}`, formValues)
+        dispatch({
+            type: 'EDIT_STREAM',
+            payload: response.data
+        })
+    }
+}
+
+export const deleteStream = (id) => {
+    return async (dispatch) => {
+        await axios.delete(`http://localhost:3001/streams/${id}`)
+        dispatch({
+            type: 'DELETE_STREAM',
+            payload: id
+        })
+    }
+}
