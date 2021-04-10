bd7d35606c427a46c9b420149602a5588f2efbb5
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-11 01:50:37 +0530

    showing edit and delete btns

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,17 +7,33 @@ class StreamList extends Component {
         this.props.fetchStreams()
     }
 
+    // for showing delete and edit btns only when the logged  in user try to get streams created by him
+    renderAdmin = (stream) => {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className='right floated content'>
+                    <button className='ui button primary'>edit</button>
+                    <button className='ui button negative'>delete</button>
+                </div>
+            )
+        }
+    }
+
     renderList = () => {
         return this.props.streams.map((stream) => {
             return (
                 <div className='item' key={stream.id}>
+                    { /*for showing btns*/}
+                    {this.renderAdmin(stream)}
+
                     <i className='large middle aligned icon camera' />
                     <div className='content'>
                         {stream.title}
+                        <div className='description'>
+                            {stream.description}
+                        </div>
                     </div>
-                    <div className='description'>
-                        {stream.description}
-                    </div>
+
                 </div>
             )
         })
@@ -39,7 +55,8 @@ const mapStateToProps = (state) => {
     // console.log(Object.values(state.streams));
     return {
         // Object.values takes an object as arg and put in an array
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
     }
 }
 
