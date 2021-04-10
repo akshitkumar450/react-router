f946e2ab1bcaae7fb0d8515537a28dab89ec9afd
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-10 14:54:09 +0530

    comments for redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 class StreamCreate extends React.Component {
     // showing error when user clicks input and clicks out without putting data in inputs
+    // touched property is used to detect whether input element is touched or not
     renderError(meta) {
         if (meta.touched && meta.error) {
             return (
@@ -16,6 +17,7 @@ class StreamCreate extends React.Component {
     }
 
     renderInput = (formProps) => {
+        // formProps are passed as the Field component runs this.rednderInput fn
         // console.log(formProps);
         // console.log(formProps.meta);  // for showing error
         return (
@@ -30,7 +32,7 @@ class StreamCreate extends React.Component {
 
     // will run when the form submit and the data of Fields will be available here
     onSubmit(formValues) {
-        // e.preventDefault() // no need with redux form ,done automatically
+        // e.preventDefault() // no need with redux form ,done automatically with handleSubmit()
         console.log(formValues);
     }
 
@@ -42,6 +44,8 @@ class StreamCreate extends React.Component {
                 <Field
                     name='title'
                     label='enter title'
+                    //label is passed as props,any time we add some props  to our Field Component redux form will does not know what to do with that label prop
+                    // so label prop will passed with renderInput fn (formProps)
                     // as the renderInput calls it will pass a set of properties as a props to that fn
                     component={this.renderInput}
                 />
@@ -52,11 +56,12 @@ class StreamCreate extends React.Component {
         )
     }
 }
-// validation
+// validation 
 // validation will occur when form in initially rendered or any interaction with form
 const validate = (formValues) => {
     const errors = {}
-    // error field name should be same as name in Field 
+    // error field name should be same as name in Field
+    // error message will passed through renderInput function
     if (!formValues.title) {
         errors.title = 'you must enter a title'
     }
@@ -66,6 +71,7 @@ const validate = (formValues) => {
     return errors
 }
 
+// reduxForm fn is same as like connect method ,which is use to get data from store to component
 export default reduxForm({
     form: 'streamCreate',
     validate: validate  // key should be form and  value can be any thing (string)
