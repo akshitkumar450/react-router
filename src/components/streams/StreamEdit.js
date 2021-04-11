e1eb7ac101769372460523bf0856ff3babbdb79e
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-11 21:56:58 +0530

    showing the stream which we want to edit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
-const StreamEdit = (props) => {
+import { fetchStream } from '../../actions'
+class StreamEdit extends React.Component {
     // when the Route component of react-router renders this component 
     // react-router-dom  passes many props to rendered component
     // console.log(props);
@@ -11,12 +11,23 @@ const StreamEdit = (props) => {
     // so streams will be undefined at begin
     // Every component needs to fetch its own data ,,and should not rely on other component data
     // that one component has loaded data we can't use that data in other component
-    console.log(props);
-    return (
-        <div>
-            StreamEdit
-        </div>
-    )
+
+    // so to get data we need to call methods so that our component can get its own data 
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+    render() {
+        // console.log(this.props.stream);
+        if (!this.props.stream) {
+            return <div>loading..</div>
+        }
+
+        return (
+            <div>
+                {this.props.stream.title}
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state, props) => {
@@ -27,4 +38,4 @@ const mapStateToProps = (state, props) => {
         stream: state.streams[props.match.params.id]
     }
 }
-export default connect(mapStateToProps)(StreamEdit)
+export default connect(mapStateToProps, { fetchStream: fetchStream })(StreamEdit)
