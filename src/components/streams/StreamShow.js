791ca9830425078c8fd8dea8e7ab036f023d8694
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-12 23:20:21 +0530

    showing data about selected stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
+class StreamShow extends React.Component {
 
-const StreamShow = (props) => {
-    console.log(props.match.params.id);
-    return (
-        <div>
-            StreamShow
-        </div>
-    )
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+
+    render() {
+        // console.log(this.props.stream);
+        // when the component will run first stream will not be available bcz componentDidmount fn runs after the component is rendered
+        if (!this.props.stream) {
+            return <div>loading..</div>
+        }
+        return (
+            <div>
+                {this.props.stream.title}
+                {this.props.stream.description}
+            </div>
+        )
+    }
 }
 
-export default StreamShow
+// fetching the stream from the redux store with the id in the url 
+const mapStateToProps = (state, props) => {
+    return {
+        stream: state.streams[props.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, {
+    fetchStream: fetchStream
+})(StreamShow)
